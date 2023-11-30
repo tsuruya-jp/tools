844ae71af79a598a7e4fba5ade69f169bc958b6f
yuri.takahashi <>
2023-11-30 14:52:17 +0900

    全データ取得

--- scraping/kobayashi-gun-shop/main.go
+++ scraping/kobayashi-gun-shop/main.go
@@ -28,6 +28,15 @@ func main() {
 	records := make([][]string, 0)
 	titles := make([]string, 0)
 	kinds := make([]string, 0)
+	calibers := make([]string, 0)
+	makers := make([]string, 0)
+	models := make([]string, 0)
+	gunHeights := make([]string, 0)
+	gunWeights := make([]string, 0)
+	pulls := make([]string, 0)
+	conditions := make([]string, 0)
+	remarks := make([]string, 0)
+	prices := make([]string, 0)
 	i := 0
 	c.OnHTML("div[id=guns_real]", func(e *colly.HTMLElement) {
 		e.ForEach("h2", func(_ int, h *colly.HTMLElement) {
@@ -44,8 +53,80 @@ func main() {
 			kinds = append(kinds, kind)
 		})
 
+		e.ForEach(".design1 > tbody > tr:nth-child(1) > td:nth-child(4)", func(_ int, h *colly.HTMLElement) {
+			caliber := h.Text
+			caliber, _ = sjis_to_utf8(caliber)
+			caliber = strings.TrimSpace(strings.TrimSuffix(caliber, "\n"))
+			calibers = append(calibers, caliber)
+		})
+
+		e.ForEach(".design1 > tbody > tr:nth-child(1) > td:nth-child(6)", func(_ int, h *colly.HTMLElement) {
+			maker := h.Text
+			maker, _ = sjis_to_utf8(maker)
+			maker = strings.TrimSpace(strings.TrimSuffix(maker, "\n"))
+			makers = append(makers, maker)
+		})
+
+		e.ForEach(".design1 > tbody > tr:nth-child(2) > td:nth-child(2)", func(_ int, h *colly.HTMLElement) {
+			model := h.Text
+			model, _ = sjis_to_utf8(model)
+			model = strings.TrimSpace(strings.TrimSuffix(model, "\n"))
+			models = append(models, model)
+		})
+
+		e.ForEach(".design1 > tbody > tr:nth-child(2) > td:nth-child(4)", func(_ int, h *colly.HTMLElement) {
+			gunHeight := h.Text
+			gunHeight, _ = sjis_to_utf8(gunHeight)
+			gunHeight = strings.TrimSpace(strings.TrimSuffix(gunHeight, "\n"))
+			gunHeights = append(gunHeights, gunHeight)
+		})
+
+		e.ForEach(".design1 > tbody > tr:nth-child(2) > td:nth-child(6)", func(_ int, h *colly.HTMLElement) {
+			gunWeight := h.Text
+			gunWeight, _ = sjis_to_utf8(gunWeight)
+			gunWeight = strings.TrimSpace(strings.TrimSuffix(gunWeight, "\n"))
+			gunWeights = append(gunWeights, gunWeight)
+		})
+
+		e.ForEach(".design1 > tbody > tr:nth-child(3) > td:nth-child(2)", func(_ int, h *colly.HTMLElement) {
+			pull := h.Text
+			pull, _ = sjis_to_utf8(pull)
+			pull = strings.TrimSpace(strings.TrimSuffix(pull, "\n"))
+			pulls = append(pulls, pull)
+		})
+
+		e.ForEach(".design1 > tbody > tr:nth-child(3) > td:nth-child(4)", func(_ int, h *colly.HTMLElement) {
+			condition := h.Text
+			condition, _ = sjis_to_utf8(condition)
+			condition = strings.TrimSpace(strings.TrimSuffix(condition, "\n"))
+			conditions = append(conditions, condition)
+		})
+
+		e.ForEach(".design1 > tbody > tr:nth-child(3) > td:nth-child(6)", func(_ int, h *colly.HTMLElement) {
+			remark := h.Text
+			remark, _ = sjis_to_utf8(remark)
+			remark = strings.TrimSpace(strings.ReplaceAll(strings.TrimSuffix(remark, "\n"), "\n", " "))
+			remarks = append(remarks, remark)
+		})
+
+		e.ForEach(".design1 > tbody > tr > td:nth-child(1)", func(_ int, h *colly.HTMLElement) {
+			price := h.Text
+			price, _ = sjis_to_utf8(price)
+			price = strings.TrimSpace(strings.TrimSuffix(price, "\n"))
+			prices = append(prices, price)
+		})
+
 		records = append(records, titles)
 		records = append(records, kinds)
+		records = append(records, calibers)
+		records = append(records, makers)
+		records = append(records, models)
+		records = append(records, gunHeights)
+		records = append(records, gunWeights)
+		records = append(records, pulls)
+		records = append(records, conditions)
+		records = append(records, remarks)
+		records = append(records, prices)
 
 	})
 
